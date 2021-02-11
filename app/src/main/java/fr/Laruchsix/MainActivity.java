59a7c5bf4e09dd752f622b273edc883b3ec2d9db
laruchsix <61274375+laruchsix@users.noreply.github.com>
2021-02-11 08:56:45 +0100

    d

--- app/src/main/java/fr/Laruchsix/MainActivity.java
+++ app/src/main/java/fr/Laruchsix/MainActivity.java
@@ -19,98 +19,79 @@ import java.io.InputStreamReader;
 public class MainActivity extends AppCompatActivity {
     private AlertDialog.Builder  popup;
     private MainActivity activity;
+    private String data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        this.data = readData();
+        if(this.data.equals("") || this.data.equals(null))
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main2);
 
-        this.activity = this;
-
-        // autre pop up
-        this.popup = new AlertDialog.Builder(this.activity);
-        this.popup.setTitle("Bonjour, merci d'avoir téléchargé MoneyMananger");
-        this.popup.setMessage("Pour continuer appuyer sur oui afin de remplir un petit formulaire pour facilier la gestion de l'application. \nPour quitter appuyer sur non.");
-        this.popup.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Intent otherActivity = new Intent(getApplicationContext(), formulairePremiereConnexion.class);
-                startActivity(otherActivity);
-                finish();
-            }
-        });
-
-        this.popup.setNegativeButton("Non", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-            }
-        });
-
-        this.popup.show();
+            this.activity = this;
 
 
-        /*
-        // initialisation du popup
-        this.popup = new Popup(this);
-        this.popup.setTitle("Bonjour, merci d'avoir téléchargé MoneyMananger");
-        this.popup.setSubTitle("Pour continuer appuyer sur oui afin de remplir un petit formulaire pour facilier la gestion de l'application. \nPour quitter appuyer sur non.");
-        this.popup.setTitleSize(20f);
+            // autre pop up
+            this.popup = new AlertDialog.Builder(this.activity);
+            this.popup.setTitle("Bonjour, merci d'avoir téléchargé MoneyMananger");
+            this.popup.setMessage("Pour continuer appuyer sur oui afin de remplir un petit formulaire pour facilier la gestion de l'application. \nPour quitter appuyer sur non.");
+            this.popup.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent otherActivity = new Intent(getApplicationContext(), formulairePremiereConnexion.class);
+                    startActivity(otherActivity);
+                    finish();
+                }
+            });
 
-        // bouton oui
-        Button yes = this.popup.getYesButton();
-        yes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent otherActivity = new Intent(getApplicationContext(), formulairePremiereConnexion.class);
-                startActivity(otherActivity);
-                finish();
-            }
-        });
+            this.popup.setNegativeButton("Non", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                }
+            });
 
-        // bouton non
-        Button no = this.popup.getNoButton();
-        no.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+            this.popup.show();
 
-        // on affiche le popup
-        this.popup.build();
-        */
-        final Button formAcc = (Button) findViewById(R.id.newAccount);
 
-        formAcc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Intent otherActivity = new Intent(getApplicationContext(), formulairePremiereConnexion.class);
-                //startActivity(otherActivity);
-                //finish();
-                readData();
-            }
-        });
+            final Button formAcc = (Button) findViewById(R.id.newAccount);
 
+            formAcc.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent otherActivity = new Intent(getApplicationContext(), formulairePremiereConnexion.class);
+                    startActivity(otherActivity);
+                    finish();
+                }
+            });
+        }
+        else
+        {
+            Intent otherActivity = new Intent(getApplicationContext(), AccountsSelect.class);
+            startActivity(otherActivity);
+            finish();
+        }
 
     }
 
 
-    private void readData() {
+    private String readData() {
         String fileName = getResources().getString(R.string.dateUser);
+        StringBuilder sb;
         try {
             // Open stream to read file.
             FileInputStream in = this.openFileInput(fileName);
 
             BufferedReader br= new BufferedReader(new InputStreamReader(in));
 
-            StringBuilder sb= new StringBuilder();
+            sb= new StringBuilder();
             String s= null;
             while((s= br.readLine())!= null)  {
                 sb.append(s).append("\n");
             }
-            ((TextView) findViewById(R.id.ret)).setText(sb.toString());
-
         } catch (Exception e) {
-            Toast.makeText(this,"Error:"+ e.getMessage(),Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this,"Error:"+ e.getMessage(),Toast.LENGTH_SHORT).show();
+            return "";
         }
+        return sb.toString();
     }
 }
