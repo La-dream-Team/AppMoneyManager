f6bbe9a22821f5a16c22a7ac32fb321035a5c2b3
N0ITUL0V3R <daniel.rodriguezhernando@gmail.com>
2021-03-25 22:25:28 +0100

    Ajout de dark mode pour Remy

--- app/src/main/java/fr/Laruchsix/Controler/MainActivity.java
+++ app/src/main/java/fr/Laruchsix/Controler/MainActivity.java
@@ -1,9 +1,11 @@
 package fr.Laruchsix.Controler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,11 +32,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // on recupère les deux texts
+        // On recupère les deux texts
         EditText edPrenom  = findViewById(R.id.edPrenom);
         EditText edNom = findViewById(R.id.edNom);
 
-        // but new
+        // On sauvegarde l'état de l'application
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        final boolean isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false);
+
+        // Quand l'utilisateur ouvre à nouveau l'app après avoir appliqué le theme sombre/normal
+        if (isDarkModeOn) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        // Button dark mode
+        Button btnDarkMode = findViewById(R.id.butDarkMode);
+        btnDarkMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(isDarkModeOn) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor.putBoolean("isDarkModeOn", false);
+                    editor.apply();
+                }
+                else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor.putBoolean("isDarkModeOn", true);
+                    editor.apply();
+                }
+            }
+        });
+
+        // Button new
         Button newuser = findViewById(R.id.butnew);
         newuser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        // button load
+        // Button load
         Button loaduser= findViewById(R.id.butload);
         loaduser.setOnClickListener(new View.OnClickListener() {
             @Override
